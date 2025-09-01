e8432693865900602f1f6ad15043eedbb642df8f
fercarvalho <fercarvalho10@gmail.com>
2025-09-01 14:06:50 -0300

    de novo o botão

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
       {/* Botão do menu para abrir */}
       {!open && (
         <button
-          className="fixed top-4 md:top-[2.5rem] left-4 z-50 bg-card p-2 rounded-lg shadow"
+          className="fixed top-[1.25rem] md:top-[2.5rem] left-4 z-50 bg-card p-2 rounded-lg shadow"
           onClick={() => setOpen(true)}
           aria-label="Abrir menu"
         >
@@ -38,7 +38,7 @@ const Sidebar = () => {
       {open && (
         <button
           onClick={() => setOpen(false)}
-          className="fixed top-4 md:top-[2.2rem] left-4 z-50 bg-card p-2 rounded-lg shadow"
+          className="fixed top-[1.25rem] md:top-[2.5rem] left-4 z-50 bg-card p-2 rounded-lg shadow"
           aria-label="Fechar menu"
         >
           <Menu className="w-6 h-6 text-foreground rotate-180" />
